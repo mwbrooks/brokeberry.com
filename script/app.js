87d4f3ab00231e59b62b6e4af89271769ef23f19
Fil Maj <filip.maj@nitobi.com>
2010-12-12 21:20:12 -0800

    Improved running cases a bit better, expanded model matching to accept ranges or exact model matches as well as lists.

--- script/app.js
+++ script/app.js
@@ -1,4 +1,17 @@
 (function() {
+    /* Case
+     * Encapsulates each test case into an atomic unit.
+     * @param model         A string or array of BlackBerry models that the case should be run on.
+     *                      A model number is four digits. A string type can contain a single 4 digit model,
+     *                      or a range with the use of a '-' between two model numbers. An array of model strings
+     *                      follows the same rule as a string but you can specify multiple models or model ranges.
+     * @param os            A string or array of BlackBerry operating system versions that the case should be run on.
+     *                      Follows the same string/array rules as the model parameter above.
+     * @param description   A string describing the test case, what causes it and what results from it.
+     * @param test          A callback function taking one parameter. The callback should contain the
+     *                      logic to perform that makes up the test case. The parameter to this function is a DOM
+     *                      element that the test case can use to inject manipulate the DOM.
+     **/
     var Case = function(model, os, description, test) {
         this.model = model;
         this.os = os;
@@ -7,15 +20,45 @@
     };
     Case.prototype = {
         matches:function(model, os) {
-            if (this.model != 'all' && model != 'all' && this.model != model) return false;
-            if (this.os != 'all' && os != 'all') {
-                var t = this.os.split('.');
-                    o = os.split('.');
-                for (var i = 0, l = t.length; i < l; i++) {
-                    if (parseInt(o[i]) != parseInt(t[i])) return false;
-                }
-            }
+            if (!this.matchesModel(model) || !this.matchesOS(os)) return false;
             return true;
+        },
+        matchesOS:function(os) {
+            var t = this.os.split('.'),
+                o = os.split('.');
+            for (var i = 0, l = t.length; i < l; i++) {
+                if (parseInt(o[i]) != parseInt(t[i])) return false;
+            }
+        },
+        matchesModel:function(model){
+            var isRange = function(str) {
+                if (str.indexOf('-') > -1) return true;
+                else return false;
+                }, inRange = function(val, range) {
+                    var spread = range.split('-'),
+                        min = parseInt(range[0]),
+                        max = parseInt(range[1]);
+                    if (typeof val == 'string') val = parseInt(val);
+                    if (val >= min && val <= max) return true;
+                    else return false;
+                }, isMatch = function(one, two) {
+                    if (isRange(one)) return inRange(one, two);
+                    else {
+                        if (parseInt(two) == parseInt(one)) return true;
+                        else return false;
+                    }
+                };
+                
+            if (typeof this.model == 'object' && this.model instanceof Array) {
+                var ret = true;
+                for (var i = 0, l = this.model.length; i < l; i++) {
+                    var c = this.model[i];
+                    if (!isMatch(c, model)) return false;
+                }
+                return true;
+            } else {
+                return isMatch(this.model, model);
+            }
         }
     };
     var isBB = false,
@@ -59,10 +102,13 @@
                     el.innerHTML += itemTemplate.split('description').join(t.description);
                     var n = el.childNodes.length,
                         cont = el.childNodes[n-1],
-                        holder = cont.childNodes[2];
-                    cont.childNodes[1].addEventListener('click', function(e) {
-                        t.test(holder);
-                    }, false);
+                        holder = cont.childNodes[2],
+                        listener = function(e) {
+                            holder.innerHTML = '';
+                            t.test(holder);
+                            this.innerHTML = 'Run me again';
+                        };
+                    cont.childNodes[1].addEventListener('click', listener, false);
                 }
             }
         };
